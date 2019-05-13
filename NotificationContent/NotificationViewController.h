091a1400830626c9d5c386369678f0a2eebe2ce8
2603160657 <2603160657@qq.com>
2019-05-13 22:39:27 +0800

    Add NotificationContent and NotificationService

---
+++ NotificationContent/NotificationViewController.h
@@ -0,0 +1,13 @@
+//
+//  NotificationViewController.h
+//  NotificationContent
+//
+//  Created by 朱志佳 on 2019/5/13.
+//  Copyright © 2019年 朱志佳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NotificationViewController : UIViewController
+
+@end
---
+++ NotificationContent/NotificationViewController.m
@@ -0,0 +1,30 @@
+//
+//  NotificationViewController.m
+//  NotificationContent
+//
+//  Created by 朱志佳 on 2019/5/13.
+//  Copyright © 2019年 朱志佳. All rights reserved.
+//
+
+#import "NotificationViewController.h"
+#import <UserNotifications/UserNotifications.h>
+#import <UserNotificationsUI/UserNotificationsUI.h>
+
+@interface NotificationViewController () <UNNotificationContentExtension>
+
+@property IBOutlet UILabel *label;
+
+@end
+
+@implementation NotificationViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any required interface initialization here.
+}
+
+- (void)didReceiveNotification:(UNNotification *)notification {
+    self.label.text = notification.request.content.body;
+}
+
+@end
